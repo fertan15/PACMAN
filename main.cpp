d4133837384aaf9da6ed83daddcad6cf774dc611
Ferlinda Tanwio <ferlinda.t24@mhs.istts.ac.id>
2024-12-19 11:54:40 +0700

    gotoxy

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
 
 using namespace std;
+
+void gotoxy(int x, int y) {
+    COORD coordinate;
+    coordinate.X = x;
+    coordinate.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinate);
+}
